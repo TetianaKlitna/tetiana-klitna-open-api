5207267474b25a0981d607e207d685d868116fd6
Tetiana Klitna <klitna.tetiana@gmail.com>
2024-09-05 14:42:41 -0700

    API fetch completed

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-fetch('https://api.github.com/users/TetianaKlitna/repos')
+function displayLocation(latitude, longitude){
+
+  let url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`;
+
+  fetch(url)
   .then(response => {
     if (!response.ok) {
       throw new Error('Request failed');
@@ -6,16 +10,52 @@ fetch('https://api.github.com/users/TetianaKlitna/repos')
     return response.json(); 
   })
   .then(data => {
-    const projectsSection = document.getElementById("projects");
-    let projectsList = projectsSection.querySelector("ul");
-    projectsList.style.listStyle = 'none';
-    for(let i = 0; i < data.length; i++){
-            let newProject = document.createElement("li");
-            newProject.innerHTML = `<a href = '${data[i].html_url}'>${data[i].name}</a>`;
-            projectsList.appendChild (newProject);
-            console.log(`${data[i].name} saccessfully added.`)
-    }
+
+    console.log(data);
+    
+    let ulItem  = document.getElementById("weather_hourly").querySelector("ul");
+    ulItem.style.listStyle = "none";
+
+    const today = new Date(data.current.time);
+    const temperature = data.current.temperature_2m;
+    const windSpeed = data.current.wind_speed_10m;
+    
+    const temperatureUnit = data.current_units.temperature_2m;
+    const windSpeedUnit = data.current_units.wind_speed_10m;
+
+    // Get the time zone offset in minutes
+    //negative value because getTimezoneOffset returns the offset to UTC
+    const offsetInMinutes = -new Date().getTimezoneOffset();
+    var local = new Date(today.getTime() + offsetInMinutes * 60000);
+  
+    const titleItem = document.createElement("li");
+    titleItem.innerHTML = `<p>Today is ${local.toLocaleDateString()}.</p>`+
+    `<p>Temperature is ${temperature} ${temperatureUnit}.</p>` + 
+    `<p>Wind speed is ${windSpeed} ${windSpeedUnit}</p>`; 
+     
+    ulItem.append(titleItem);
+
+
   })
   .catch(error => {
     console.error('An error occurred:', error);
   });
+}
+
+function successCallback(position){
+
+  var lat = position.coords.latitude;
+  var long = position.coords.longitude;
+
+  displayLocation(lat, long);
+}
+
+function errorsCallBack(err){
+    console.error(`ERROR(${err.code}): ${err.message}`);
+}
+
+if(navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(successCallback, errorsCallBack);
+}else{
+  console.error("Your browser does not support geolocation.");
+}
